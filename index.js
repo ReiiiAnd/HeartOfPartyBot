326cb6192c6f0e982bda8b4056420b67111a6527
ReiiiAnd <turbokirichenko@mail.ru>
2020-12-27 20:11:26 +0000

    debugging.

--- index.js
+++ index.js
@@ -2,34 +2,37 @@ var TelegramBot = require('node-telegram-bot-api');
 
 // Устанавливаем токен, который выдавал нам бот
 var token = '1422948086:AAGOfViDOu7ooXR0d09wYUhb--nFKJWwG4M';
-// Включить опрос сервера. Бот должен обращаться к серверу Telegram, чтобы получать актуальную информацию
-// Подробнее: https://core.telegram.org/bots/api#getupdates
-var bot = new TelegramBot(token, { polling: true });
 
-function TestRegex(str, regex){
-    return str.search(regex) != -1;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+function ExpectMatch(str){
+    const pattern = /[A-Za-z - A-Za-z]/
+    if(str.search(pattern) == -1){
+        return "sorry";
+    }
+    return str
 }
 
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием, то есть "Hello World!")
-bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
-    var resp = match[1]; // Получаем текст после /echo
-    var pattern = /[A-Za-z - A-Za-z]/
-    var error = 'sorry.. but I can\'t understand you'
-    if(TestRegex(resp, pattern)){
-        bot.sendMessage(fromId, resp);
-    }
-    else{
-        bot.sendMessage(fromId, error);
-    }
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  var resp = match[1]; // the captured "whatever"
+    resp = ExpectMatch(resp)
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
-// Простая команда без параметров
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id; // Берем ID чата (не отправителя)
-   // var fromId = msg.from.id;
-    // Фотография может быть: путь к файлу, поток (stream) или параметр file_id
-    var photo = 'cats.png'; // в папке с ботом должен быть файл "cats.png"
-   // bot.sendMessage(fromId, "Maybe you want this...")
-    bot.sendPhoto(chatId, photo, { caption: 'Is that what you\'re looking for' });
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
 });
