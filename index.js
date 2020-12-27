aa5c320b8fb9fa4459dd063721e4bd77ec3faf6d
ReiiiAnd <turbokirichenko@mail.ru>
2020-12-27 22:16:10 +0000

    Final_commit

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
 var TelegramBot = require('node-telegram-bot-api');
-
+var Stream = require('fs');
 // Устанавливаем токен, который выдавал нам бот
 var token = '1422948086:AAGOfViDOu7ooXR0d09wYUhb--nFKJWwG4M';
 
-
+var flag = false
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-function ExpectMatch(str){
-    const pattern = /[A-Za-z - A-Za-z]/
-    if(str.search(pattern) == -1){
-        return "sorry";
-    }
-    return str
+function AddNodeToFile(str, user_id){
+	var record = '\n' + user_id + ' : ' + str
+	Stream.appendFileSync("music_list.txt", record)
 }
 
+function ExpectMatch(str, user_id){
+	var patterns = new RegExp("[a-zA-Z0-9\s] - [a-zA-Z0-9\s]")
+    if(str.search(patterns) != -1){
+	AddNodeToFile(str, user_id)
+        return 'nice!'
+    }
+    return 'sorry.. but I can\'t understand you((('
+}
+
+
+bot.onText(/\/start/, (msg) => {
+	flag = true
+	const chatId = msg.from.id
+	bot.sendMessage(chatId, 'Hi, mom... I need get \/help from you')
+});
+
+bot.onText(/\/help/, (msg) => {
+	flag = true
+	const chatId = msg.from.id
+	bot.sendMessage(chatId, 'Yaa! send me a title of your favorite song!\n like this:\n\/send Lynyrd Skynyrd - Sweet Home Alabama')
+});
+
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/send (.+)/, (msg, match) => {
+	flag = true
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
-  const chatId = msg.chat.id;
+  const chatId = msg.from.id;
   var resp = match[1]; // the captured "whatever"
-    resp = ExpectMatch(resp)
+    resp = ExpectMatch(resp, chatId)
+//   resp = 'sorry'
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+	const chatId = msg.chat.id
+	if(flag){
+		bot.sendMessage(chatId, '...')
+	}
+	else{
+		bot.sendMessage(chatId, ')')
+	}
 });
