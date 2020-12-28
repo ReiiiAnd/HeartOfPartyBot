be03529c17231cf9421ffb26f41861fd72b0bce1
Your Name <turbokirichenko@mail.ru>
2020-12-28 10:58:19 -0800

    update1

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 var TelegramBot = require('node-telegram-bot-api');
 var Stream = require('fs');
-// Устанавливаем токен, который выдавал нам бот
 var token = '1422948086:AAGOfViDOu7ooXR0d09wYUhb--nFKJWwG4M';
 
 var flag = false
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-function AddNodeToFile(str, user_id){
+function AddNodeToFile(nameOfFile, str, user_id){
 	var record = '\n' + user_id + ' : ' + str
-	Stream.appendFileSync("music_list.txt", record)
+	Stream.appendFileSync(nameOfFile, record)
+}
+
+function SearchContent(nameOfFile, pattern){
+	var output
+	Stream.readFile(nameOfFile, 'utf8', (err, data)=> {
+		if(err) throw err
+
+		output = data;
+	})
+
+	var rez = output.match(pattern)
+	return rez[0]
+}
+
+function FileContent(nameOfFile){
+	var output
+	Stream.readFile(nameOfFile, 'utf8', (err, data)=> {
+		if(err) throw err
+
+		output = data;
+	})
+
+	return output
 }
 
 function ExpectMatch(str, user_id){
-	var patterns = new RegExp("[a-zA-Z0-9\s] - [a-zA-Z0-9\s]")
-    if(str.search(patterns) != -1){
-	AddNodeToFile(str, user_id)
+	var pattern = new RegExp("[a-zA-Z0-9' _]-[a-zA-Z0-9' _]")
+    if(str.search(pattern) != -1){
+    	var fileName = "music_list.txt"
+    	var heard = SearchContent(fileName, pattern)
+    	if(heard){
+    		return 'yes I know this song'
+    	}
+		AddNodeToFile(fileName, str , user_id)
         return 'nice!'
     }
     return 'sorry.. but I can\'t understand you((('
@@ -34,25 +60,28 @@ bot.onText(/\/help/, (msg) => {
 	bot.sendMessage(chatId, 'Yaa! send me a title of your favorite song!\n like this:\n\/send Lynyrd Skynyrd - Sweet Home Alabama')
 });
 
-// Matches "/echo [whatever]"
+bot.onText(/\/list/, (msg) => {
+	flag = true
+	const chatId = msg.from.id
+	var answer = FileContent("music_list.txt")
+	bot.sendMessage(chatId, answer)
+})
+
+
 bot.onText(/\/send (.+)/, (msg, match) => {
 	flag = true
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
-  const chatId = msg.from.id;
-  var resp = match[1]; // the captured "whatever"
+	const chatId = msg.from.id;
+	var resp = match[1]; 
     resp = ExpectMatch(resp, chatId)
-//   resp = 'sorry'
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+	bot.sendMessage(chatId, resp);
 });
 
 bot.on('message', (msg) => {
 	const chatId = msg.chat.id
 	if(flag){
 		bot.sendMessage(chatId, '...')
+		flag = false
 	}
 	else{
 		bot.sendMessage(chatId, ')')
